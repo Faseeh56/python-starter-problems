b405e00fd1cd00a2c589d2582c29e989d4b1930e
Faseeh <179562644+Faseeh56@users.noreply.github.com>
2026-09-11 07:19:21 +0500

    1/8 program done here

---
+++ Programs/01_personal_info.py
@@ -0,0 +1,13 @@
+Name = "Faseeh Qamar"
+Age = 20
+University = "Minhaj University Lahore"
+Degree = "BSCS"
+Semester  = 7
+Favorite_language = "Python"
+
+print("Name:", Name)
+print("Age:", Age)  
+print("University:", University)
+print("Degree:", Degree)
+print("Semester:", Semester)
+print("Favorite Programming Language:", Favorite_language)
