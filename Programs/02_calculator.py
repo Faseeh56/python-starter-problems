cf17e4d2e94ced17c73fd9c3a1482decbf40c6fe
Faseeh <179562644+Faseeh56@users.noreply.github.com>
2026-09-11 07:29:47 +0500

    Created Screenshots folder and 2/8 problem solved

---
+++ Programs/02_calculator.py
@@ -0,0 +1,14 @@
+num_a = int(input("Enter the first number: "))
+num_b = int(input("Enter the second number: "))
+
+Addition = num_a + num_b
+Subtraction = num_a - num_b
+Multiplication = num_a * num_b
+Division = num_a / num_b
+Remainder = num_a % num_b
+
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
+print("Remainder: ", Remainder)
